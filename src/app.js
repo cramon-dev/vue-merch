58bf51f757eb3f17be558505a1bb86581fe1f116
Christian Ramon <christian.ramon@ipayables.com>
2019-06-24 17:43:06 -0600

    Completed lesson 5
    Added a way to "add" a product to a cart. Added the ability to view
    variants of our single product by mousing over each color.

--- src/app.js
+++ src/app.js
@@ -6,18 +6,34 @@ let app = new Vue({
     description: 'A pair of socks. Nothing fancy',
     inventory: 100,
     details: ["80% cotton", "20% polyester", "Gender neutral"],
+    cart: 0,
     variants: [
       {
         variantId: 1,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: './img/vmSocks-green.jpg'
       },
       {
         variantId: 2,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: './img/vmSocks-blue.jpg'
       }
     ],
     sizes: [
       "S", "M", "L"
     ]
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if(this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
