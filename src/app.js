630338ff25493005284d590ad8cff0337089e1d5
Christian Ramon <christian.ramon@ipayables.com>
2019-06-24 09:36:30 -0600

    Completed lesson 4
    Added details, colors, and sizes (for the end of lesson challenge) to
    demonstrate list rendering

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ let app = new Vue({
     image: 'img/vmSocks-green.jpg',
     description: 'A pair of socks. Nothing fancy',
     inventory: 100,
-    onSale: true
+    details: ["80% cotton", "20% polyester", "Gender neutral"],
+    variants: [
+      {
+        variantId: 1,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: [
+      "S", "M", "L"
+    ]
   }
 });
