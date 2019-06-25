e980b66aeb6e2f252630c2fdc85863bc09b29bd8
Christian Ramon <christian.ramon@ipayables.com>
2019-06-24 20:08:02 -0600

    Completed lesson 7
    Added more computed properties such as dynamic images and inventory for
    certain variants of a product.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 let app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: 'img/vmSocks-green.jpg',
     description: 'A pair of socks. Nothing fancy',
-    inventory: 15,
     details: ["80% cotton", "20% polyester", "Gender neutral"],
+    onSale: true,
     cart: 0,
+    selectedVariant: 0,
     variants: [
       {
         variantId: 1,
         variantColor: 'green',
-        variantImage: './img/vmSocks-green.jpg'
+        variantImage: './img/vmSocks-green.jpg',
+        variantQuantity: 15
       },
       {
         variantId: 2,
         variantColor: 'blue',
-        variantImage: './img/vmSocks-blue.jpg'
+        variantImage: './img/vmSocks-blue.jpg',
+        variantQuantity: 10
       }
     ],
     sizes: [
@@ -25,7 +28,22 @@ let app = new Vue({
   },
   computed: {
     inStock() {
-      return this.inventory > 0;
+      return this.variants[this.selectedVariant].variantQuantity > 0;
+    },
+    variantInventory() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    title() {
+      return `${this.brand} ${this.product}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    emptyCart() {
+      return this.cart === 0;
+    },
+    saleMessage() {
+      return `${this.brand} ${this.product} is currently on sale; hurry and get yours before it's too late!`
     }
   },
   methods: {
@@ -37,8 +55,8 @@ let app = new Vue({
         this.cart -= 1;
       }
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
     }
   }
 });
